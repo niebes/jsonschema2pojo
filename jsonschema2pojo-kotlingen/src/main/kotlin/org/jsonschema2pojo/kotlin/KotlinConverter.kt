f9ee03a8aca57a3ff82d3bb44b4c3a705acb11e8
Mario Niebes <github@niebes.net>
2021-01-24 15:27:46 +0100

    basic data class

--- jsonschema2pojo-kotlingen/src/main/kotlin/org/jsonschema2pojo/kotlin/KotlinConverter.kt
+++ jsonschema2pojo-kotlingen/src/main/kotlin/org/jsonschema2pojo/kotlin/KotlinConverter.kt
@@ -1,20 +1,28 @@
 package org.jsonschema2pojo.kotlin
 
 import com.squareup.kotlinpoet.AnnotationSpec
+import com.squareup.kotlinpoet.BOOLEAN
+import com.squareup.kotlinpoet.BYTE
 import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.DOUBLE
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.INT
 import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.LONG
 import com.squareup.kotlinpoet.ParameterSpec
 import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.SHORT
+import com.squareup.kotlinpoet.STRING
 import com.squareup.kotlinpoet.TypeSpec
-import com.squareup.kotlinpoet.TypeVariableName
 import com.sun.codemodel.JAnnotationUse
 import com.sun.codemodel.JAnnotationValue
+import com.sun.codemodel.JClass
 import com.sun.codemodel.JFieldVar
 import com.sun.codemodel.JFormatter
 import com.sun.codemodel.JGenerable
 import com.sun.codemodel.JPackage
+import com.sun.codemodel.JType
 import java.io.CharArrayWriter
 
 
@@ -29,10 +37,8 @@ class KotlinConverter {
             .primaryConstructor(
                 FunSpec.constructorBuilder().apply {
                     clazz.fields().entries.forEach { (name, fieldType) ->
-                        val erasure = fieldType.type().erasure()
                         addParameter(
-                            ParameterSpec.builder(name, fullQualifiedNameToPoetClassName(erasure.fullName()))
-                                .addModifiers(KModifier.PUBLIC)
+                            ParameterSpec.builder(name, jTypeToPoetClassName(fieldType.type()))
                                 .build()
                         )
                     }
@@ -40,14 +46,11 @@ class KotlinConverter {
                 }.build()
             ).apply {
                 clazz.fields().entries.forEach { (name, fieldType) ->
-                    val erasure = fieldType.type().erasure()
                     addProperty(
-                        PropertySpec.builder(name, fullQualifiedNameToPoetClassName(erasure.fullName()))
+                        PropertySpec.builder(name, jTypeToPoetClassName(fieldType.type()))
                             .initializer(name).apply {
                                 val propertySpec = this
-                                toAnnotationSpec(fieldType).onEach {
-                                    propertySpec.addAnnotation(it)
-                                }
+                                toAnnotationSpec(fieldType).onEach(propertySpec::addAnnotation)
                             }
                             .build()
                     )
@@ -63,7 +66,7 @@ class KotlinConverter {
 
     private fun toAnnotationSpec(jFieldVar: JFieldVar): List<AnnotationSpec> =
         jFieldVar.annotations().map { jAnnotationUse ->
-            val type = fullQualifiedNameToPoetClassName(jAnnotationUse.annotationClass.fullName())
+            val type = jClassToPoetClassName(jAnnotationUse.annotationClass)
             AnnotationSpec.builder(type)
                 .apply {
                     jAnnotationUse.annotations().map { (key, value) ->
@@ -87,9 +90,23 @@ class KotlinConverter {
             mapOf()
         }
 
-        private fun fullQualifiedNameToPoetClassName(fullName: String): ClassName {
-            val fullNameSplit = fullName.split(".")
-            return ClassName(fullNameSplit.dropLast(1).joinToString("."), fullNameSplit.last())
+        private fun jTypeToPoetClassName(jType: JType): ClassName =
+            fullQualifiedNameToPoetClassName(jType.erasure().fullName())
+
+        private fun jClassToPoetClassName(jClass: JClass): ClassName =
+            fullQualifiedNameToPoetClassName(jClass.fullName())
+
+        private fun fullQualifiedNameToPoetClassName(fullName: String): ClassName = when (fullName) {
+            "java.lang.String" -> STRING
+            "java.lang.Integer" -> INT
+            "java.lang.Double" -> DOUBLE
+            "java.lang.Byte" -> BYTE
+            "java.lang.Short" -> SHORT
+            "java.lang.Long" -> LONG
+            "java.lang.Boolean" -> BOOLEAN
+            else -> {
+                ClassName.bestGuess(fullName)
+            }
         }
     }
 }
